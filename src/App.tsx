d3d81ea863040503090d71941a0b9dad462417af
40moaz <amoaz14109@gmail.com>
2024-05-23 23:43:26 +0200

    sdsd

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
 import About from "./components/about/About"
 import Contact from "./components/contact/Contact"
+import { FaLinkedin } from "react-icons/fa";
 import Footer from "./components/footer/Footer"
+import { IoLogoFacebook } from 'react-icons/io';
 import Nav from "./components/navbar/Nav"
 import Projects from "./components/projects/Projects"
-import { FaLinkedin } from "react-icons/fa";
-import { IoLogoGithub } from "react-icons/io";
-
 
 const App = () => {
   return (
@@ -16,8 +15,8 @@ const App = () => {
       <Contact />
       <Footer />
       <div className=" absolute bottom-[100px] sm:bottom-[150px] left-[40%] md:fixed md:left-5 md:top-[40%] flex gap-5 md:flex-col" >
-        <a href="https://www.linkedin.com/in/sherif-altiby-38b350229/" className="text-4xl cursor-pointer md:block text-dark  hover:text-btnColor duration-200  "><FaLinkedin /></a>
-        <a href="https://github.com/Sherif-altiby" className="text-4xl cursor-pointer md:block text-dark  hover:text-btnColor duration-200  "><IoLogoGithub /></a>
+        <a href="https://www.linkedin.com/in/moaz-ali-9764662a3/" className="text-4xl cursor-pointer md:block text-dark  hover:text-btnColor duration-200  "><FaLinkedin /></a>
+        <a href="https://www.facebook.com/moaz.programmer/" className="text-4xl cursor-pointer md:block text-dark  hover:text-btnColor duration-200  "><IoLogoFacebook /></a>
       </div>
     </div>
   )
--- src/components/contact/Contact.tsx
+++ src/components/contact/Contact.tsx
@@ -1,7 +1,9 @@
-import { FaRegCopy } from "react-icons/fa6";
-import Aos from "aos";
 import "aos/dist/aos.css"
+
 import { useEffect, useState } from "react";
+
+import Aos from "aos";
+import { FaRegCopy } from "react-icons/fa6";
 
 const Contact = () => {
  
@@ -26,7 +28,7 @@ const Contact = () => {
         <div className="container m-auto pb-6">
             <h1 className="w-fit m-auto" data-aos="fade-right"  data-aos-duration="1500"  > Get In Touch </h1>
             <div className="p-5 shadow-lg flex flex-col w-[95%] sm:w-auto sm:flex-row justify-around mt-10 rounded-lg h-[150px] items-center bg-white lg:w-[950px] ml-auto mr-auto" data-aos="fade-right"  data-aos-duration="1500"  >
-                <p className="text-dark text-md sm:text-xl flex sm:gap-5" >sherifahmed852002@gmail.com <span className="cursor-pointer relative w-12 h-6 flex items-center justify-center text-center " onClick={() => copyEmail()} >  {showText ? <span className="text-btnColor text-[10px] sm:text-[15px]" > Copied  </span> : <FaRegCopy/>}</span> </p>
+                <p className="text-dark text-md sm:text-xl flex sm:gap-5" >amoaz14109@gmail.com <span className="cursor-pointer relative w-12 h-6 flex items-center justify-center text-center " onClick={() => copyEmail()} >  {showText ? <span className="text-btnColor text-[10px] sm:text-[15px]" > Copied  </span> : <FaRegCopy/>}</span> </p>
                 <div> <a className="no-underline bg-btnColor text-light cursor-pointer rounded-lg py-3 px-4 hover:bg-[#126ffa] duration-200 text-xl" href="mailto:sherifahmed852002@gmail.com"> Send Email </a> </div>
             </div>
         </div>
